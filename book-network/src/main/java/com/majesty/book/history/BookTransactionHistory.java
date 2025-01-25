a752462c6d806f4ee2a671dd1eef560abe47797e
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-25 20:55:01 -0300

    Feat: Add book transaction history layer

---
+++ book-network/src/main/java/com/majesty/book/history/BookTransactionHistory.java
@@ -0,0 +1,31 @@
+package com.majesty.book.history;
+
+import com.majesty.book.book.Book;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @Column(name = "user_id")
+    private String userId;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
