48d211c33c9ab140038b051971dce044e3bb7220
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-25 15:58:59 -0300

    Initial commit

---
+++ book-nework/src/main/java/com/majesty/book/BookNeworkApplication.java
@@ -0,0 +1,13 @@
+package com.majesty.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNeworkApplication.class, args);
+	}
+
+}
