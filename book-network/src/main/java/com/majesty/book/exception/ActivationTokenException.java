0534f9e1214d6c980c018b145c934dd96471c664
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-03 00:39:57 -0300

    Feat: Add exception feature

---
+++ book-network/src/main/java/com/majesty/book/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.majesty.book.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
