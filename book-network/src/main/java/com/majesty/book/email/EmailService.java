dba183f11b3d907321db7e9fa1756601c07ef617
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-03 00:38:56 -0300

    Feat: Add email feature

--- book-network/src/main/java/com/majesty/book/email/EmailService.java
+++ book-network/src/main/java/com/majesty/book/email/EmailService.java
@@ -50,7 +50,7 @@ public class EmailService {
         Context context = new Context();
         context.setVariables(properties);
 
-        helper.setFrom("contact@majesty.com");
+        helper.setFrom("contact@aliboucoding.com");
         helper.setTo(to);
         helper.setSubject(subject);
 
